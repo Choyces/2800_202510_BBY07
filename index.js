0fe0a7751a8463fece09f4ea17e4a37a94d075fe
Qian Zhang <qzhang116@my.bcit.ca>
2025-05-22 09:15:26 -0700

    create logout button

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ app.listen(port, function () {
     console.log("Example app listening on port " + port + "!");
 });
 
-app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   // console.log(`Incoming request: ${req.method} ${req.url}`);
+//   next();
+// });
